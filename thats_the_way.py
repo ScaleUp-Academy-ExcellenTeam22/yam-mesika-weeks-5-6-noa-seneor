f3c21041e5a26ac938cd92cd0a79700c33e8349a
noa-seneor <100723117+noa-seneor@users.noreply.github.com>
2022-03-21 09:51:07 +0100

    Update thats_the_way.py

--- thats_the_way.py
+++ thats_the_way.py
@@ -1,6 +1,16 @@
 import os
-_path = input("enter your directory path")
-if os.path.isdir(_path):
-    print(list(filter(lambda x: x.startswith("deep"), os.listdir(_path))))
-else:
-    print("not a directory")
+
+
+def files_deep():
+    """
+    asks the user a directory path
+    :return: list of all files in the directory that starts with deep
+    """
+    _path = input("enter your directory path: ")
+    if os.path.isdir(_path):
+        return [x for x in os.listdir(_path) if x.startswith("deep")]
+    else:
+        return "not a directory"
+
+
+print(files_deep())
