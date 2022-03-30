482e2bd5dfaab0b66472ecd5d7867f06f36bfc1e
noa-seneor <100723117+noa-seneor@users.noreply.github.com>
2022-03-30 09:59:40 +0200

    Update piece_of_cake.py

--- piece_of_cake.py
+++ piece_of_cake.py
@@ -1,8 +1,9 @@
 def get_recipe_price(prices, optionals=[], **ingredients):
     """
-    receives as parameter a dictionary of ingredients : price for 100gr ,
-    optional ingredients that we ignore and quantity for all ingredients
-    :return: total price
+    :param prices: dictionary of ingredient key price value (100gr)
+    :param optionals: list of optional ingredients
+    :param ingredients: ingredient quantity
+    :return: total price without optionals
     """
-    total_lst = [ingredients.get(ing)*prices.get(ing) for ing in ingredients if ing not in optionals]
+    total_lst = [ingredients.get(ing) * prices.get(ing) for ing in ingredients if ing not in optionals]
     return sum(total_lst)//100
